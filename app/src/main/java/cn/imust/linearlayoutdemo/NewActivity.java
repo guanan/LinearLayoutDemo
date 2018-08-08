48133a3e347bb1e71d3cf9206024e23e61bac3ba
coder-appen <791121155@qq.com>
2018-08-08 16:56:18 +0800

    线性布局Demo

---
+++ app/src/main/java/cn/imust/linearlayoutdemo/NewActivity.java
@@ -0,0 +1,17 @@
+package cn.imust.linearlayoutdemo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * @author Appen
+ * @time 2018/8/8 16:54
+ */
+public class NewActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
